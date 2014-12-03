e62e06363441ff4630aeb9eb896dcec571d54a84
leoyuchuan <leoyuchuan@gmail.com>
2014-12-02 22:56:33 -0600

    all services done w/o testing

--- Server/services/login.php
+++ Server/services/login.php
@@ -1,73 +1,59 @@
 <?php
 /*
-	Name: login.php
-	Description: Input User Information & Return validation information. (Using Post)
-	Input: username, password, region = {cn, us}
-	Output: $validation = {verified, error_message}
-	As following format
-	
-	<?xml version="1.0" encoding="UTF-8"?>
-	<result>
-		<message>$validation</message>
-	</result>
+Name: login.php
+Description: Input User Information & Return validation information. (Using Post)
+Input: username, password, region = {cn, us}
+Output: $validation = {verified, error_message}
+As following format
+
+<?xml version="1.0" encoding="UTF-8"?>
+<result>
+        <message>$validation</message>
+</result>
 */	
-        require_once '../vendor/autoload.php';
-        require_once '../generated-conf/config.php';
-	$xml = '<?xml version="1.0" encoding="UTF-8"?><result><message>%s</message></result>';
-	
-        /*
-         * Fetch Input Data
-         */
-        // $username = $_POST['username'];
-	// $password = $_POST['password'];
-	// $region = $_POST['region'];
-	$username = $_GET['username'];
-	$password = $_GET['password'];
-	$region = $_GET['region'];
-	
-        /*
-         *  Process Input Data
-	*/
-	$region = strtolower($region);
-	if(preg_match("/^[a-zA-Z][a-zA-Z0-9]*$/", $username) !== 1){
-		echo sprintf($xml, 'wrong username');
-		return;
-	}
-	if(preg_match("/^[a-zA-Z][a-zA-Z0-9]*$/", $password) !== 1){
-		echo sprintf($xml, 'wrong password');
-		return;
-	}
-	if(preg_match("/^(cn)|(us)$/", $region) !== 1){
-		echo sprintf($xml, 'wrong region');
-		return;
-	}
-        
-        /*
-         * Login Processing
-         */
-        if(strtolower(trim($region)) === "us"){
-            $conn = Propel\Runtime\Propel::getConnection('us_topspace');
-            $user = UserQuery::create()->findByArray(array('Username'=>$username, 'Password'=>$password), $conn);
-            if($user->count() > 0){
-                echo sprintf($xml,'verified');
-                return;
-            }
-            else{
-                echo sprintf($xml,"user not found");
-                return;
-            }
-        }
-        if(strtolower(trim($region)) === "cn"){
-            $conn = Propel\Runtime\Propel::getConnection('cn_topspace');
-            $user = UserQuery::create()->findByArray(array('Username'=>$username, 'Password'=>$password), $conn);
-            if($user->count() > 0){
-                echo sprintf($xml,'verified');
-                return;
-            }
-            else{
-                echo sprintf($xml,"user not found");
-                return;
-            }
-        }
+require_once '../vendor/autoload.php';
+require_once '../generated-conf/config.php';
+$xml = '<?xml version="1.0" encoding="UTF-8"?><result><message>%s</message></result>';
+
+/*
+ * Fetch Input Data
+ */
+// $username = $_POST['username'];
+// $password = $_POST['password'];
+// $region = $_POST['region'];
+$username = $_GET['username'];
+$password = $_GET['password'];
+$region = $_GET['region'];
+
+/*
+ *  Process Input Data & Get Connection Base on Region
+*/
+$region = strtolower($region);
+if(preg_match("/^[a-zA-Z][a-zA-Z0-9]*$/", $username) !== 1){
+        echo sprintf($xml, 'Wrong Username');
+        return;
+}
+if(preg_match("/^[a-zA-Z][a-zA-Z0-9]*$/", $password) !== 1){
+        echo sprintf($xml, 'Wrong Password');
+        return;
+}
+if(preg_match("/^(cn)|(us)$/", $region) !== 1){
+        echo sprintf($xml, 'Wrong Region');
+        return;
+}
+$conn = Propel\Runtime\Propel::getConnection(strtolower(trim($region)).'_topspace');
+
+/*
+ * Login Processing
+ */
+$user = UserQuery::create()->findByArray(array('Username'=>$username, 'Password'=>$password), $conn);
+if($user->count() > 0){
+    echo sprintf($xml,'Verified');
+    return;
+}
+else{
+    echo sprintf($xml,"Wrong Pair");
+    return;
+}
 
 ?>
--- Server/services/register.php
+++ Server/services/register.php
@@ -1,82 +1,60 @@
 <?php
 /*
-	Name: register.php
-	Description: Input User Information & Return registration information. (Using Post)
-	Input: username, password, region = {cn, us}, ...
-	Output: $message = {success, error_message}
-	As following format
-	
-	<?xml version="1.0" encoding="UTF-8"?>
-	<result>
-		<message>$message</message>
-	</result>
+Name: register.php
+Description: Input User Information & Return registration information. (Using Post)
+Input: username, password, region = {cn, us}, ...
+Output: $message = {success, error_message}
+As following format
+
+<?xml version="1.0" encoding="UTF-8"?>
+<result>
+        <message>$message</message>
+</result>
 */	
-        require_once '../vendor/autoload.php';
-        require_once '../generated-conf/config.php';
-	$xml = '<?xml version="1.0" encoding="UTF-8"?><result><message>%s</message></result>';
-	// $username = $_POST['username'];
-	// $password = $_POST['password'];
-	// $region = $_POST['region'];
-	$username = $_GET['username'];
-	$password = $_GET['password'];
-	$region = $_GET['region'];
+require_once '../vendor/autoload.php';
+require_once '../generated-conf/config.php';
+$xml = '<?xml version="1.0" encoding="UTF-8"?><result><message>%s</message></result>';
+// $username = $_POST['username'];
+// $password = $_POST['password'];
+// $region = $_POST['region'];
+$username = $_GET['username'];
+$password = $_GET['password'];
+$region = $_GET['region'];
 
-	/*
-		Process Input Data
-	*/
-	$region = strtolower($region);
-	if(preg_match("/^[a-zA-Z][a-zA-Z0-9]*$/", $username) !== 1){
-		echo sprintf($xml, 'wrong username');
-		return;
-	}
-	if(preg_match("/^[a-zA-Z][a-zA-Z0-9]*$/", $password) !== 1){
-		echo sprintf($xml, 'wrong password');
-		return;
-	}
-	if(preg_match("/^(cn)|(us)$/", $region) !== 1){
-		echo sprintf($xml, 'wrong region');
-		return;
-	}
-        /*
-         * Registration Processing
-         */
-        if(strtolower(trim($region)) === "us"){
-            $conn = Propel\Runtime\Propel::getConnection('us_topspace');
-            $user = UserQuery::create()->findByArray(array('Username'=>$username), $conn);
-            if($user->count() > 0){
-                echo sprintf($xml,'username exists');
-                return;
-            }
-            $user = new User();
-            $user->setUsername($username);
-            $user->setPassword($password);
-            try{
-                $user->save($conn);
-                echo sprintf($xml,'success');
-                return;
-            }  catch (Exception $e){
-                echo sprintf($xml,$e->getMessage());
-                return;
-            }
-        }
-        if(strtolower(trim($region)) === "cn"){
-            $conn = Propel\Runtime\Propel::getConnection('cn_topspace');
-            $user = UserQuery::create()->findByArray(array('Username'=>$username), $conn);
-            if($user->count() > 0){
-                echo sprintf($xml,'username exists');
-                return;
-            }
-            $user = new User();
-            $user->setUsername($username);
-            $user->setPassword($password);
-            try{
-                $user->save($conn);
-                echo sprintf($xml,'success');
-                return;
-            }  catch (Exception $e){
-                echo sprintf($xml,$e->getMessage());
-                return;
-            }
-        }
-
+/*
+ * Process Input Data & Get Connection Base on Region
+*/
+$region = strtolower($region);
+if(preg_match("/^[a-zA-Z][a-zA-Z0-9]*$/", $username) !== 1){
+        echo sprintf($xml, 'Wrong Username');
+        return;
+}
+if(preg_match("/^[a-zA-Z][a-zA-Z0-9]*$/", $password) !== 1){
+        echo sprintf($xml, 'Wrong Password');
+        return;
+}
+if(preg_match("/^(cn)|(us)$/", $region) !== 1){
+        echo sprintf($xml, 'Wrong Region');
+        return;
+}
+$conn = Propel\Runtime\Propel::getConnection(strtolower(trim($region)).'_topspace');
+/*
+ * Registration Processing
+ */ 
+$users = UserQuery::create()->findByArray(array('Username'=>$username), $conn);
+if($users->count() > 0){
+    echo sprintf($xml,'User Exists');
+    return;
+}
+$user = new User();
+$user->setUsername($username);
+$user->setPassword($password);
+try{
+    $user->save($conn);
+    echo sprintf($xml,'Success');
+    return;
+}  catch (Exception $e){
+    echo sprintf($xml,'Fail');
+    return;
+}
 ?>
