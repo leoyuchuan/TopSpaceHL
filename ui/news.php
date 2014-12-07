9ce51d0a9474cb448786cb53e77683423dc676fd
leoyuchuan <leoyuchuan@gmail.com>
2014-12-06 21:47:11 -0600

    final?

--- ui/news.php
+++ ui/news.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-    $status = $_SESSION['status'];
-    if($status !== 'online'){
-        echo '<script>window.location = "login.php"</script>';
-        return;
-    }
+$status = $_SESSION['status'];
+if ($status !== 'online') {
+    echo '<script>window.location = "login.php"</script>';
+    return;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -74,97 +74,129 @@ session_start();
             <div class="row">
                 <div class="col-lg-12">
                     <?php
-                        require_once 'HTTP/Request2.php';
-                        session_start();
-                        $region = $_SESSION['region'];
-                        $news_id = $_GET['nid'];
-                        if (preg_match("/^[1-9][0-9]*$/", $news_id) != 1) {
+                    require_once 'HTTP/Request2.php';
+                    session_start();
+                    $region = $_SESSION['region'];
+                    $news_id = $_GET['nid'];
+                    if (preg_match("/^[1-9][0-9]*$/", $news_id) != 1) {
+                        echo "<script>window.location='home.php'</script>";
+                        return;
+                    }
+
+
+                    $memcached = new Memcached();
+                    $memcached->addServer('localhost', 11211);
+                    $key = "newsid" . $news_id . $region;
+                    $body = $memcached->get($key);
+                    if ($body) {
+                        if (preg_match("/^.*<message>.*$/", $body) > 0)
                             echo "<script>window.location='home.php'</script>";
-                            return;
-                        }
-
-
-                        $memcached = new Memcached();
-                        $memcached->addServer('localhost', 11211);
-                        $key = "newsid".$news_id.$region;
-                        $body = $memcached->get($key);
-                        if ($body) {
+                        $xml = simplexml_load_string($body);
+                    } else {
+                        $r = new Http_Request2('http://www.webserver' . rand(1, 2) . '.com/news.php');
+                        $r->setMethod(HTTP_Request2::METHOD_POST);
+                        $r->addPostParameter(array('o' => 'byid', 'news_id' => $news_id, 'region' => $region));
+                        try {
+                            $body = $r->send()->getBody();
                             if (preg_match("/^.*<message>.*$/", $body) > 0)
                                 echo "<script>window.location='home.php'</script>";
+                            $memcached->set($key, $body, 10);
                             $xml = simplexml_load_string($body);
-                        } else {
-                            $r = new Http_Request2('http://www.webserver' . rand(1, 2) . '.com/news.php');
-                            $r->setMethod(HTTP_Request2::METHOD_POST);
-                            $r->addPostParameter(array('o' => 'byid', 'news_id' => $news_id, 'region' => $region));
-                            try {
-                                $body = $r->send()->getBody();
-                                if (preg_match("/^.*<message>.*$/", $body) > 0)
-                                    echo "<script>window.location='home.php'</script>";
-                                $memcached->set($key, $body, 10);
-                                $xml = simplexml_load_string($body);
-                            } catch (Exception $ex) {
-                                echo $ex->getMessage();
-                            }
+                        } catch (Exception $ex) {
+                            echo $ex->getMessage();
                         }
-                        $new = $xml->news[0];
-                        $id = (string) $new->news_id[0];
-                        $date = $new->date[0];
-                        $title = $new->title[0];
-                        $content = $new->content[0];
+                    }
+                    $new = $xml->news[0];
+                    $id = (string) $new->news_id[0];
+                    $date = $new->date[0];
+                    $title = $new->title[0];
+                    $content = $new->content[0];
 //                        echo "<div><span>$id</span><span>$title</span><span>$date</span></div>";
-                        // echo "id:$id | title:$title | date:$date<br/>$content</br>";
-                        // echo "<br/><br/>";
-                        // echo "<a href='./phpscript/subprocess.php?nid=$id'>subscribe</a>    ";
-                        // echo "<br/><br/>";
+                    // echo "id:$id | title:$title | date:$date<br/>$content</br>";
+                    // echo "<br/><br/>";
+                    // echo "<a href='./phpscript/subprocess.php?nid=$id'>subscribe</a>    ";
+                    // echo "<br/><br/>";
 
-                        echo "<table class='table'><caption>Recent News List</caption> 
+                    echo "<table class='table'><caption>Recent News List</caption> 
                               <thead><tr><th>ID</th><th>Title</th><th>Date</th><th>Subscribe</th></tr><thead>
                              ";
-                        echo "<tbody><tr><td>$id</td><td><a href='news.php?nid=$id'>$title</a></td><td>$date</td>
+                    echo "<tbody><tr><td>$id</td><td><a href='news.php?nid=$id'>$title</a></td><td>$date</td>
                               <td><a href='./phpscript/subprocess.php?nid=$id'>subscribe</a></td></tr></tbody><br/>";
-                        echo "</table>";
-                        echo "<br/>$content</br>";
+                    echo "</table>";
+                    echo "<br/>$content</br>";
 
-                        $memcached = new Memcached();
-                        $memcached->addServer('localhost', 11211);
-                        $key = "commentnewsid".$id.$region;
-                        $body = $memcached->get($key);
-                        if ($body) {
+                    $memcached = new Memcached();
+                    $memcached->addServer('localhost', 11211);
+                    $key = "commentnewsid" . $id . $region;
+                    $body = $memcached->get($key);
+                    if ($body) {
+                        $xml = simplexml_load_string($body);
+                    } else {
+                        $r = new Http_Request2('http://www.webserver' . rand(1, 2) . '.com/viewcomment.php');
+                        $r->setMethod(HTTP_Request2::METHOD_POST);
+                        $r->addPostParameter(array('news_id' => $id, 'region' => $region));
+                        try {
+                            $body = $r->send()->getBody();
+                            $memcached->set($key, $body, 10);
                             $xml = simplexml_load_string($body);
-                        } else {
-                            $r = new Http_Request2('http://www.webserver' . rand(1, 2) . '.com/viewcomment.php');
-                            $r->setMethod(HTTP_Request2::METHOD_POST);
-                            $r->addPostParameter(array('news_id' => $id, 'region' => $region));
-                            try {
-                                $body = $r->send()->getBody();
-                                $memcached->set($key, $body, 10);
-                                $xml = simplexml_load_string($body);
-                            } catch (Exception $ex) {
-                                echo $ex->getMessage();
-                            }
+                        } catch (Exception $ex) {
+                            echo $ex->getMessage();
                         }
-                        echo "<table class='table'><caption><span class='label label-primary'>Comments Box</span></caption> 
+                    }
+                    echo "<table class='table'><caption><span class='label label-primary'>Comments Box</span></caption> 
                               <thead><tr><th>Username</th><th>Comments</th></tr><thead><tbody>
                               ";
-                        foreach ($xml->comment as $comment) {
-                            $uname = $comment->username;
-                            $content = $comment->content;
-                            echo "$uname: $content<br/>";
-                            echo "<tr><td>$uname</td>
+                    foreach ($xml->comment as $comment) {
+                        $uname = $comment->username;
+                        $content = $comment->content;
+                        echo "<tr><td>$uname</td>
                                   <td>$content</td></tr>";
+                    }
+                    echo "</tbody><br/></table>";
+                    ?>
+                    <!--Post comments -->
+                    <form role="form" action="javascript:void(0);" name="comments">
+                        <div class="form-group">
+                            <label for="name">Comments</label>
+                            <input type="text" class="form-control" id="comment" 
+                                   placeholder="Enter Comments">
+                        </div>
+                        <button type="submit" class="btn btn-default" onclick="postcomment()">Submit</button>
+                    </form>
+                    <script>
+                        function postcomment() {
+                            var comment = document.getElementById('comment').value;
+                            if (comment.trim().length <= 5) {
+                                alert("Comment too short!");
+                                return;
+                            }
+<?php
+$nid = $_GET['nid'];
+echo "var news_id = $nid;";
+?>
+                            var xmlhttp;
+                            if (window.XMLHttpRequest)
+                            {// code for IE7+, Firefox, Chrome, Opera, Safari
+                                xmlhttp = new XMLHttpRequest();
+                            }
+                            else
+                            {// code for IE6, IE5
+                                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                            }
+                            xmlhttp.onreadystatechange = function ()
+                            {
+                                if (xmlhttp.responseText.trim() === "Success") {
+                                    window.location.reload();
+                                    return;
+                                }
+                            };
+                            xmlhttp.open("POST", "./phpscript/pcomments.php", true);
+                            var params = "comment=" + comment + "&nid=" + news_id;
+                            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                            xmlhttp.setRequestHeader("Accept-Encoding", "gzip,deflat");
+                            xmlhttp.send(params);
                         }
-                        echo "</tbody><br/></table>";
-
-                        ?>
-                        <!--Post comments -->
-                        <form role="form" action="" name="comments">
-                               <div class="form-group">
-                                  <label for="name">Comments</label>
-                                  <input type="text" class="form-control" id="name" 
-                                     placeholder="Enter Comments">
-                               </div>
-                               <button type="submit" class="btn btn-default">Submit</button>
-                        </form> ;
+                    </script>
 
                 </div>
             </div>
